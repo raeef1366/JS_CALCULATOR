95f1aa5079475e885a98f2a697e56d1822716f8c
raeef1366 <115214483+raeef1366@users.noreply.github.com>
2023-01-17 13:46:20 +0400

    calc_changes

--- Js_Porject/public/index.js
+++ Js_Porject/public/index.js
@@ -2,7 +2,7 @@ const inputANode = document.querySelector(".js-input-a");
 const inputBNode = document.querySelector(".js-input-b");
 const selectOperationNode = document.querySelector(".js-select-operation");
 const btnResultNode = document.querySelector(".js-btn-result");
-const outputNode = document.querySelector(".js-output");
+const outputNode = document.querySelector(".output");
 
 btnResultNode.addEventListener("click", function() {
   const a = Number(inputANode.value);
